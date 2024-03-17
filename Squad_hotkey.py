946cf93cceb284370cf0f04494d79b3a1bfcf27a
zdhs123 <59812046+zdhs123@users.noreply.github.com>
2024-03-17 13:00:07 +0800

    Update Squad_hotkey.py

--- Squad_hotkey.py
+++ Squad_hotkey.py
@@ -1,25 +1,33 @@
-# from threading import Thread,Timer
+from threading import Thread,Timer
 import time
 import keyboard
-# import mouse
+import mouse
 from mouse._mouse_event import ButtonEvent, MoveEvent, WheelEvent, LEFT, RIGHT, MIDDLE, X, X2, UP, DOWN, DOUBLE
 
 
 #放大镜
-mag_key=58#58=caps lock,57=space
+# mag_key='/'#58=caps lock,57=space
 mag_triggrt_key=74#74=Num-键
-x_key='x'
-c_key='c'
-z_key='z'
 #小地图
 smap_key='m'
 #大地图    
 lmap_key='tab'
+#密位调节
+x_key='x'
+c_key='c'
+# t_key='t'
+# f_key='f'
+# z_key='z'
+# ctrl_key='ctrl'
+shift_key='shift'
 #标志位
+# if_mag_key_pressed=False    
+if_mouse_R_pressed=False
+# if_mouse_R_pressed_and_maged=False
 if_smap_key_pressed=False
 if_lmap_key_pressed=False
-if_mag_key_pressed=False
 if_x_key_pressed=False
+if_shift_key_pressed=False
 if_magnified=False
 
 keyboard.block_key(91)#禁用win键，防止跳出游戏
@@ -28,30 +36,27 @@ keyboard.block_key(91)#禁用win键，防止跳出游戏
 
 while True:
     time.sleep(0.001)
-#放大镜
-    if (not if_mag_key_pressed) and keyboard.is_pressed(mag_key):
-        if_mag_key_pressed=True
-        if not if_magnified:
-            keyboard.send(mag_triggrt_key)
+#Shift键放大
+    if (not if_shift_key_pressed) and keyboard.is_pressed(shift_key):
+        if_shift_key_pressed=True
+        shift_pr_time=time.time()
+        # keyboard.send(mag_triggrt_key)
+    elif if_shift_key_pressed and (not keyboard.is_pressed(shift_key)):
+        if_shift_key_pressed=False
+        if time.time()-shift_pr_time<0.15:
+            keyboard.send('*')
             if_magnified=True
-    elif if_mag_key_pressed and (not keyboard.is_pressed(mag_key)):
-        if_mag_key_pressed=False
-        if if_magnified:
-            keyboard.send(mag_triggrt_key)
-            if_magnified=False
-    # elif if_magnified:
-    #     if keyboard.is_pressed('='):      
-    #         keyboard.send('*')
-    if if_magnified:
-        if (not if_x_key_pressed) and (keyboard.is_pressed(z_key) or keyboard.is_pressed(x_key) or keyboard.is_pressed(c_key)):
-            if_x_key_pressed=True
-            keyboard.send(mag_triggrt_key)
-        elif if_x_key_pressed and (not (keyboard.is_pressed(z_key) or keyboard.is_pressed(x_key) or keyboard.is_pressed(c_key))):
-            if_x_key_pressed=False
-            keyboard.send(mag_triggrt_key)
-#限制鼠标活动范围
-    # if mouse.get_position()[1]<350:
-    #     mouse.move(1269, 769, absolute=True, duration=0)
+        elif time.time()-shift_pr_time<0.5 :
+            if if_magnified:
+                keyboard.send(mag_triggrt_key)
+                if_magnified=False
+#密位调节
+    if (not if_x_key_pressed) and (keyboard.is_pressed(x_key) or keyboard.is_pressed(c_key)) and if_magnified:
+        if_x_key_pressed=True
+        keyboard.send(mag_triggrt_key)
+    elif if_x_key_pressed and (not (keyboard.is_pressed(x_key) or keyboard.is_pressed(c_key))) and if_magnified:
+        if_x_key_pressed=False
+        keyboard.send(mag_triggrt_key)
 #小地图
     if (not if_smap_key_pressed) and keyboard.is_pressed(smap_key):
         if_smap_key_pressed=True
